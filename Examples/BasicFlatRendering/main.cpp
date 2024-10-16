9ff112a1e617cec4877aaf905df84f360b908afa
Ao Gao <ao.gao@tum.de>
2024-10-16 23:02:16 +0200

    CHORE: Update example

--- Examples/BasicFlatRendering/main.cpp
+++ Examples/BasicFlatRendering/main.cpp
@@ -10,23 +10,29 @@ int main()
 
     Transform vikingRoomTransform;
     vikingRoomTransform
-        .Translate(glm::vec3(-0.5, -0.5, 0))
+        .Translate(glm::vec3(0, 0, 0))
         .Rotate(glm::vec3(1, 0, 0), -90)
         .Rotate(glm::vec3(0, 0, 1), -90)
-        .Scale(glm::vec3(0.5, 0.5, 0.5));
+        .Scale(glm::vec3(0.4, 0.4, 0.4));
+
+    XRLib::Scene::Mesh* avocadoPtr{ nullptr };
 
     xrLib.SceneBackend()
         .LoadMeshAsync({ "../resources/Duck.glb", "",
-            {glm::vec3(0,0.5,0), glm::vec3(0,1,0), -40, glm::vec3(0.2,0.2,0.2)} })
+            {glm::vec3(-1,0,0), glm::vec3(0,1,0), -40, glm::vec3(0.2,0.2,0.2)} })
         .LoadMeshAsync({ "../resources/viking_room.obj", "../resources/viking_room.png", vikingRoomTransform })
-        .LoadMeshAsync({ "../resources/Avocado.glb", "", {glm::vec3(0,0,0), glm::vec3(0,0,0), 0, glm::vec3(5,5,5)} })
+        .LoadMeshAsync({ "../resources/Avocado.glb", "", {glm::vec3(1,0,0), glm::vec3(0,0,0), 0, glm::vec3(5,5,5)} }).BindToPointer(avocadoPtr)
+        .LoadMeshAsync({ "../resources/Suzanne.gltf", "../resources/Suzanne_BaseColor.png",
+            {glm::vec3(2,0,0), glm::vec3(0,0,0), 0, glm::vec3(0.2, 0.2,0.2)} })
         .WaitForAllMeshesToLoad();
+
+    xrLib.SceneBackend().CameraTransform().Translate(glm::vec3(0, -0.3, 0));
 
     xrLib.Init(false);
     while (!xrLib.ShouldStop()) {
 
         // rotate the avacado
-        xrLib.SceneBackend().Meshes()[2].transform.Rotate(glm::vec3(0, 1, 0), 0.1f);
+        avocadoPtr->transform.Rotate(glm::vec3(0, 1, 0), 0.1f);
 
         xrLib.Run();
     }
