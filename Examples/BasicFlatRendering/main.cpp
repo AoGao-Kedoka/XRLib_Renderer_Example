03aa87e18022a5300c3498f4931bce4646c8d732
Ao Gao <ao.gao@tum.de>
2024-11-04 16:40:52 +0100

    FEAT: Add plane to basic example

--- Examples/BasicFlatRendering/main.cpp
+++ Examples/BasicFlatRendering/main.cpp
@@ -10,33 +10,33 @@ int main()
 
     Transform vikingRoomTransform;
     vikingRoomTransform
-        .Translate(glm::vec3(0, 0, 0))
+        .Translate(glm::vec3(0, 0.1, 0))
         .Rotate(glm::vec3(1, 0, 0), -90)
         .Rotate(glm::vec3(0, 0, 1), -90)
         .Scale(glm::vec3(0.4, 0.4, 0.4));
+
+    Transform defaultTransform;
 
     XRLib::Scene::Mesh* avocadoPtr{ nullptr };
 
     // load meshes
     xrLib.SceneBackend()
         .LoadMeshAsync({ "../resources/Duck.glb", "",
-            {glm::vec3(-1,0,0), glm::vec3(0,1,0), -40, glm::vec3(0.2,0.2,0.2)} })
+            {glm::vec3(-1,0.1,0), glm::vec3(0,1,0), -40, glm::vec3(0.2,0.2,0.2)} })
         .LoadMeshAsync({ "../resources/viking_room.obj", "../resources/viking_room.png", vikingRoomTransform })
-        .LoadMeshAsync({ "../resources/Avocado.glb", "", {glm::vec3(1,0,0), glm::vec3(0,0,0), 0, glm::vec3(5,5,5)} }).BindToPointer(avocadoPtr)
+        .LoadMeshAsync({ "../resources/Avocado.glb", "", {glm::vec3(1,0,0), glm::vec3(0,0.1,0), 0, glm::vec3(5,5,5)} }).BindToPointer(avocadoPtr)
         .LoadMeshAsync({ "../resources/Suzanne.gltf", "../resources/Suzanne_BaseColor.png",
-            {glm::vec3(2,0,0), glm::vec3(0,0,0), 0, glm::vec3(0.2, 0.2,0.2)} })
+            {glm::vec3(2,0.5,0), glm::vec3(0,0,0), 0, glm::vec3(0.2, 0.2,0.2)} })
+        .LoadMeshAsync({"../resources/plane.obj", "", defaultTransform})
         .WaitForAllMeshesToLoad();
 
     // update camera position
     xrLib.SceneBackend().CameraTransform().Translate(glm::vec3(0, -0.3, 0));
 
     // add lights
-    Transform firstLightTransform;
-    Transform secondLightTransform;
-    secondLightTransform.Translate(glm::vec3(1, 0, 0));
-    xrLib.SceneBackend()
-        .AddLights({ firstLightTransform, glm::vec4(1.0f,1.0f,1.0f,1.0f), 0.5 })
-        .AddLights({ secondLightTransform, glm::vec4(1.0f, 1.0f, 1.0f, 1.0f), 0.5 });
+    Transform light;
+    light.Translate(glm::vec3(0, 1, 0));
+    xrLib.SceneBackend().AddLights({ light, glm::vec4(1.0f,1.0f,1.0f,1.0f), 0.5 });
 
 
     xrLib.Init(false);
