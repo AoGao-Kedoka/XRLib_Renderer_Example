0a3da4d76a88ec9bdb91550618abb88690b31daa
Ao Gao <ao.gao@tum.de>
2024-12-19 23:21:52 +0100

    CHORE: Update examples

--- Examples/XRRendering/main.cpp
+++ Examples/XRRendering/main.cpp
@@ -18,19 +18,15 @@ int main()
         .Rotate(glm::vec3(0, 0, 1), -90)
         .Scale(glm::vec3(0.4, 0.4, 0.4));
 
+
     XRLib::Transform defaultTransform;
 
 
     // load meshes
     xrLib.SceneBackend()
-        .LoadMeshAsync({ "../resources/Duck.glb", "",
-                {glm::vec3(-1,0.1,-2), glm::vec3(0,1,0), -40, glm::vec3(0.2,0.2,0.2)} })
-    .LoadMeshAsync({ "../resources/viking_room.obj", "../resources/viking_room.png", vikingRoomTransform })
-        .LoadMeshAsync({ "../resources/Avocado.glb", "", {glm::vec3(1,0,-2), glm::vec3(0,0.1,0), 0, glm::vec3(5,5,5)} })
-        .LoadMeshAsync({ "../resources/Suzanne.gltf", "../resources/Suzanne_BaseColor.png",
-                {glm::vec3(2,0.5,-2), glm::vec3(0,0,0), 0, glm::vec3(0.2, 0.2,0.2)} })
-    .LoadMeshAsync({"../resources/plane.obj", "", defaultTransform})
-        .WaitForAllMeshesToLoad();
+        .LoadMeshAsync({ "../resources/plane.obj", "", defaultTransform })
+        .LoadMeshAsync({ "../resources/leftHand.glb", "", defaultTransform }).AttachLeftControllerPose()
+        .LoadMeshAsync({ "../resources/rightHand.glb", "", defaultTransform }).AttachRightControllerPose();
 
     xrLib.Init();
     while (!xrLib.ShouldStop()) {
