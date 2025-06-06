2e4e63d6ed3bc2901efc752621e2a724359de89c
Ao Gao <ao.gao@tum.de>
2025-06-07 00:11:03 +0200

    CHORE: Camera updates

--- Examples/BasicFlatRendering/main.cpp
+++ Examples/BasicFlatRendering/main.cpp
@@ -4,57 +4,57 @@ XRLib::XRLib xrLib;
 
 int main()
 {
-    xrLib.SetVersionNumber(1, 0, 0)
-        .SetApplicationName("Demo application")
-        .EnableValidationLayer();
+	xrLib.SetVersionNumber(1, 0, 0)
+			.SetApplicationName("Demo application")
+			.EnableValidationLayer();
 
-    XRLib::Transform vikingRoomTransform;
-    vikingRoomTransform
-        .Translate(glm::vec3(0, 0.1, -2))
-        .Rotate(glm::vec3(1, 0, 0), -90)
-        .Rotate(glm::vec3(0, 0, 1), -90)
-        .Scale(glm::vec3(0.4, 0.4, 0.4));
+	XRLib::Transform vikingRoomTransform;
+	vikingRoomTransform
+			.Translate(glm::vec3(0, 0.1, -2))
+			.Rotate(glm::vec3(1, 0, 0), -90)
+			.Rotate(glm::vec3(0, 0, 1), -90)
+			.Scale(glm::vec3(0.4, 0.4, 0.4));
 
-    XRLib::Transform defaultTransform;
+	XRLib::Transform defaultTransform;
 
-    XRLib::Entity* avocadoPtr{ nullptr };
+	XRLib::Entity* avocadoPtr{nullptr};
 
-    // load meshes
-    xrLib.SceneBackend()
-        .LoadMeshAsync({ .meshPath = "../resources/Duck.glb",
-                         .transform = {glm::vec3(-1,0.1,-2), glm::vec3(0,1,0), -40, glm::vec3(0.2,0.2,0.2)} })
-        .LoadMeshAsync({ .meshPath = "../resources/viking_room.obj",
-                         .transform = vikingRoomTransform,
-                         .diffuseTexturePath = "../resources/viking_room.png" })
-        .LoadMeshAsyncWithBinding({ .meshPath = "../resources/Avocado.glb",
-                                    .transform = {glm::vec3(1,0,-2), glm::vec3(0,0.1,0), 0, glm::vec3(5,5,5)} },
-            avocadoPtr)
-        .LoadMeshAsync({ .meshPath = "../resources/Suzanne.gltf",
-                         .transform = {glm::vec3(2,0.5,-2), glm::vec3(0,0,0), 0, glm::vec3(0.2, 0.2,0.2)},
-                         .diffuseTexturePath = "../resources/Suzanne_BaseColor.png"})
-        .LoadMeshAsync({.meshPath =  "../resources/sponza.glb", .transform = defaultTransform })
-        .WaitForAllMeshesToLoad();
+	// load meshes
+	xrLib.SceneBackend()
+			.LoadMeshAsync({.meshPath = "../resources/Duck.glb",
+							.transform = {glm::vec3(-1, 0.1, -2), glm::vec3(0, 1, 0), -40, glm::vec3(0.2, 0.2, 0.2)}})
+			.LoadMeshAsync({.meshPath = "../resources/viking_room.obj",
+							.transform = vikingRoomTransform,
+							.diffuseTexturePath = "../resources/viking_room.png"})
+			.LoadMeshAsyncWithBinding({.meshPath = "../resources/Avocado.glb",
+									   .transform = {glm::vec3(1, 0, -2), glm::vec3(0, 0.1, 0), 0, glm::vec3(5, 5, 5)}},
+									  avocadoPtr)
+			.LoadMeshAsync({.meshPath = "../resources/Suzanne.gltf",
+							.transform = {glm::vec3(2, 0.5, -2), glm::vec3(0, 0, 0), 0, glm::vec3(0.2, 0.2, 0.2)},
+							.diffuseTexturePath = "../resources/Suzanne_BaseColor.png"})
+			.LoadMeshAsync({.meshPath = "../resources/sponza.glb", .transform = defaultTransform})
+			.WaitForAllMeshesToLoad();
 
-    // update camera position
-    xrLib.SceneBackend().MainCamera()->GetLocalTransform().Translate(glm::vec3(0, -1.3, 0));
+	// update camera position
+	xrLib.SceneBackend().MainCamera()->GetLocalTransform().Translate(glm::vec3(0, 1.3, 0));
 
-    // add lights
-    XRLib::Transform lightTransform;
-    lightTransform.Translate(glm::vec3(0, 1, 0));
-    xrLib.SceneBackend().AddPointLights(lightTransform, glm::vec4(1.0f, 1.0f, 1.0f, 1.0f), 0.5, avocadoPtr);
+	// add lights
+	XRLib::Transform lightTransform;
+	lightTransform.Translate(glm::vec3(0, 1, 0));
+	xrLib.SceneBackend().AddPointLights(lightTransform, glm::vec4(1.0f, 1.0f, 1.0f, 1.0f), 0.5, avocadoPtr);
 
-    xrLib.Init(false);
+	xrLib.Init(false);
 
-    LOGGER(LOGGER::DEBUG) << xrLib.SceneBackend();
+	LOGGER(LOGGER::DEBUG) << xrLib.SceneBackend();
 
-    while (!xrLib.ShouldStop()) {
+	while (!xrLib.ShouldStop()) {
 
-        // rotate the avacado
-        if (avocadoPtr)
-            avocadoPtr->GetLocalTransform().Rotate(glm::vec3(0, 1, 0), 0.1f);
+		// rotate the avacado
+		if (avocadoPtr)
+			avocadoPtr->GetLocalTransform().Rotate(glm::vec3(0, 1, 0), 0.1f);
 
-        xrLib.Run();
-    }
+		xrLib.Run();
+	}
 
-    return 0;
+	return 0;
 }
--- Examples/CustomRenderPass/main.cpp
+++ Examples/CustomRenderPass/main.cpp
@@ -1,25 +1,30 @@
 #include "customPass.h"
 
-int main(){
-    XRLib::XRLib xrLib;
+/**
+ * @brief Simple example of setting up custom renderpass
+ * For simplicity, buffer updates are not implemented
+*/
+int main()
+{
+	XRLib::XRLib xrLib;
 
-    xrLib.SetVersionNumber(1, 0, 0)
-        .SetApplicationName("Demo application")
-        .EnableValidationLayer()
-        .SceneBackend()
-        .LoadMeshAsync(
-                {.meshPath = "../resources/sponza.glb",
-                .transform = {glm::vec3(0,0,0), glm::vec3(0, 0, 0), 0,
-                glm::vec3(0.2, 0.2, 0.2)}})
-        .WaitForAllMeshesToLoad();
+	xrLib.SetVersionNumber(1, 0, 0)
+			.SetApplicationName("Demo application")
+			.EnableValidationLayer()
+			.SceneBackend()
+			.LoadMeshAsync(
+					{.meshPath = "../resources/sponza.glb",
+					 .transform = {glm::vec3(0, 0, 0), glm::vec3(0, 0, 0), 0,
+								   glm::vec3(0.2, 0.2, 0.2)}})
+			.WaitForAllMeshesToLoad();
 
-    // prepare custom renderpass
-    auto customRenderBehavior = std::make_unique<CustomPass>(xrLib.GetVkCore(), xrLib.SceneBackend(), &xrLib.RenderBackend().RenderPasses, false);
-    xrLib.Init(false, std::move(customRenderBehavior));
+	// prepare custom renderpass
+	auto customRenderBehavior = std::make_unique<CustomPass>(xrLib.GetVkCore(), xrLib.SceneBackend(), &xrLib.RenderBackend().RenderPasses, false);
+	xrLib.Init(false, std::move(customRenderBehavior));
 
-    while (!xrLib.ShouldStop()){
-        xrLib.Run();
-    }
+	while (!xrLib.ShouldStop()) {
+		xrLib.Run();
+	}
 
-    return 0;
+	return 0;
 }
